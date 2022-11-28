242efa40b2648afb4f7aee38926c723e7141719e
shakilla1 <shakilshifat65@gmail.com>
2022-11-28 20:10:31 +0600

    added navbar

--- src/Shared/Navbar.js
+++ src/Shared/Navbar.js
@@ -18,14 +18,7 @@ const Navbar = () => {
                         <label tabIndex={0} className="btn btn-ghost  lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-slate-700 rounded-box w-52">
-                            <li><Link to='home'>Home</Link></li>
-                            <li><Link to='addservice'>Add Product</Link></li>
-                            <li><Link to='myproduct'>My Product</Link></li>
-                            <li><Link to='blog'>Blog</Link></li>
-                            <li><Link to='dashboard'>Dashboard</Link></li>
 
-                        </ul>
                     </div>
 
                     <span><img className='h-10 w-10 rounded-sm  ' src='https://i.ibb.co/dbz5pW2/images-2.jpg' alt="" /></span>
