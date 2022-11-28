c936e658bc3c34f7c216034171cc22d78fa2d54b
shakilla1 <shakilshifat65@gmail.com>
2022-11-28 20:11:55 +0600

    add nav home section

--- src/Shared/Navbar.js
+++ src/Shared/Navbar.js
@@ -65,8 +65,7 @@ const Navbar = () => {
                                         </>
                                         :
                                         <>
-                                            <li><Link to='/login'>Login</Link></li>
-                                            <li><Link to='/register'>Register</Link></li>
+
                                         </>
                                 }
 
