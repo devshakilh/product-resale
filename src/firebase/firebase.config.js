bec8be82936ea3f1ddd39599076a95bb09b896df
shakilla1 <shakilshifat65@gmail.com>
2022-11-28 20:18:38 +0600

    complete website

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,24 +3,7 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_apiKey,
-//     authDomain: process.env.REACT_APP_authDomain,
-//     projectId: process.env.REACT_APP_projectId,
-//     storageBucket: process.env.REACT_APP_storageBucket,
-//     messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//     appId: process.env.REACT_APP_appId,
 
-// };
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAEAZYROujh2978ct04Gfx0dYMZT4aF6ds",
-//     authDomain: "refrigerator-seller.firebaseapp.com",
-//     projectId: "refrigerator-seller",
-//     storageBucket: "refrigerator-seller.appspot.com",
-//     messagingSenderId: "574942857389",
-//     appId: "1:574942857389:web:2d641f9b556e722ede5e24"
-// };
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
     authDomain: process.env.REACT_APP_authDomain,
